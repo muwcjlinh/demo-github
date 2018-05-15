35404e15a1205e2bcae6e862e7e12e4719cad02e
dungnv <vietdungnguyen0412@gmail.com>
2018-05-15 14:26:01 +0700

    fix db

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
-  'database': 'mongodb://demodb0412:perfect2@ds123770.mlab.com:23770/heroku_35nqqq9z',
+  'database': 'mongodb://heroku_5rp5zj87:s7gag83m4p3eb6cqvtmlp8s8k1@ds223760.mlab.com:23760/heroku_5rp5zj87',
   // Set port
   'port': process.env.PORT || 3000
 }
