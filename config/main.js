9ba715804fbcc00828d13d9275d4e8e611073be7
dungnv <vietdungnguyen0412@gmail.com>
2018-05-15 13:52:49 +0700

    update db

--- config/main.js
+++ config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   'secret': 'super secret passphrase',
   // Database connection information
-  'database': 'mongodb://mongo/sky',
+  'database': 'mongodb://demodb0412:perfect2@ds123770.mlab.com:23770/heroku_35nqqq9z',
   // Set port
   'port': process.env.PORT || 3000
 }
